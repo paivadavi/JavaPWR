955933cd44516567a9015911057c68b0922587c0
Davi Paiva <50916238+paivadavi@users.noreply.github.com>
2021-01-31 16:19:53 +0100

    I added two lines more for testing purposes

--- Lab05/src/lab05/Main.java
+++ Lab05/src/lab05/Main.java
@@ -9,9 +9,12 @@ public class Main {
 	public static void main(String[] args) {
 		int[]a1 = {1,9};
 		int[]a2 = {8,2};
+		int[]a3 = {1,2,3,4,5};
 		System.out.println(Arrays.toString(ArrayFunctions.toNumberArray(123)));//works
 		System.out.println(Arrays.toString(ArrayFunctions.toNumberArray("7634")));//works
 		System.out.println(Arrays.toString(ArrayFunctions.addNumberArrays(a1, a2)));
+		System.out.println(Arrays.toString(ArrayFunctions.addNumberArrays(a1, a3)));
+		System.out.println("Printing test");
 		
 	}
 
